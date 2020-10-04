c302a365e82d679fcacc74b3f1ad9858c029e2a5
WeslleyWar <55511759+WeslleyWar@users.noreply.github.com>
2020-10-04 18:41:29 -0300

    Add files via upload

---
+++ Locadora/Locadora/Views/frmRegistroDevolucao.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Locadora.Views
+{
+    /// <summary>
+    /// Interaction logic for frmRegistroDevolucao.xaml
+    /// </summary>
+    public partial class frmRegistroDevolucao : Window
+    {
+        public frmRegistroDevolucao()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCancelar_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
