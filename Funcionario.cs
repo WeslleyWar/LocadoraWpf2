5d833f294336b99769e62241e0031aec654c4be6
WeslleyWar <55511759+WeslleyWar@users.noreply.github.com>
2020-10-05 20:55:52 -0300

    Funcionario
    Crud Funcionario

---
+++ Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    [Table("Funcionarios")]
+    class Funcionario : Pessoa
+    {   
+
+        public Funcionario()
+        {
+            CriadoEm = DateTime.Now;
+        }
+
+        [Key]
+        public int IdFuncionario { get; set; }
+        public DateTime DataContratacao { get; set; }
+        public string Cargo { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public float Salario { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
---
+++ FuncionarioDAO.cs
@@ -0,0 +1,42 @@
+﻿using Locadora.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora.DAL
+{
+    class FuncionarioDAO : ClienteDAO
+    {
+        //Funcionario deixa de Editar ou remover informações cadastradas.
+        private static Context ctx =
+            SingletonContext.GetInstance();
+        //private static Context ctx = new Context();
+        //Funcionarios
+        public static Funcionario BuscarFuncionarioPorId(Funcionario id)
+        {
+            return ctx.Funcionarios.Find(id);
+        }
+        public static bool CadastrarFuncionario(Funcionario f)
+        {
+            if (BuscarFuncionarioPorCpf(f) == null)
+            {
+                ctx.Funcionarios.Add(f);
+                ctx.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public static Funcionario BuscarFuncionarioPorCpf(Funcionario f)
+        {
+            return ctx.Funcionarios.FirstOrDefault(x => x.Cpf.Equals(f.Cpf));
+        }
+
+        public static List<Funcionario> ListarFuncionarios()
+        {
+            return ctx.Funcionarios.ToList();
+        }
+    }
+}
