5b1051c8319c8fd6d077d73cc976344230ddbe43
WeslleyWar <55511759+WeslleyWar@users.noreply.github.com>
2020-10-05 22:23:10 -0300

    Funcionario e veiculo
    Formulario de veiculo e funcionario

---
+++ Locadora/Locadora/Views/frmCadFuncionario.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Locadora.DAL;
+using Locadora.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Locadora.Views
+{
+    /// <summary>
+    /// Interaction logic for CadFuncionario.xaml
+    /// </summary>
+    public partial class frmCadFuncionario : Window
+    {
+        public frmCadFuncionario()
+        {
+            InitializeComponent();
+            txtCriadoEm.Text = DateTime.Now.ToString();
+        }
+
+        private void LimparFormulario()
+        {
+            txtNome.Clear();
+            txtGenero.Clear();
+            txtIdade.Clear();
+            txtRg.Clear();
+            txtSalario.Clear();
+            txtEmail.Clear();
+            //txtDataNascimento.Clear();
+            txtCriadoEm.Clear();
+            txtCpf.Clear();
+            //txtContratacao.Clear();
+            txtCargo.Clear();
+            txtCep.Clear();
+            txtCep.Clear();
+            txtCnh.Clear();            
+        }
+
+        private void btnSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            Funcionario f = new Funcionario
+            {
+                Nome = txtNome.Text,
+                Genero = Convert.ToString(txtGenero.Text),
+                Idade = Convert.ToInt32(Convert.ToString(txtIdade.Text)),
+                Cpf = Convert.ToString(txtCpf.Text),
+                DataContratacao = Convert.ToDateTime(Convert.ToString(dtpContratacao.Text)),
+                Rg = Convert.ToString(txtRg.Text),
+                Cnh = Convert.ToString(txtCnh.Text),
+                DataNascimento = Convert.ToDateTime(Convert.ToString(dtpNascimento.Text)),
+                Email = Convert.ToString(txtEmail.Text),
+                Cep = Convert.ToString(txtCep.Text),
+                Cargo = Convert.ToString(txtCargo.Text),
+                Salario = float.Parse(Convert.ToString(txtSalario.Text)),
+                CriadoEm = DateTime.Now
+
+            };
+            if (FuncionarioDAO.CadastrarFuncionario(f))
+            {
+                MessageBox.Show("Funcionario cadastrado!", "Locador", MessageBoxButton.OK, MessageBoxImage.Information);
+                LimparFormulario();
+            }
+            else
+            {
+                MessageBox.Show("Funcionario já cadastrado!", "Locador", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
--- Locadora/Locadora/Views/frmCadastroVeiculo.xaml.cs
+++ Locadora/Locadora/Views/frmCadastroVeiculo.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Locadora.DAL;
+using Locadora.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,9 +26,7 @@ namespace Locadora.Views
             InitializeComponent();
         }
 
-        
-
-        private void btnCancelarCadastroVeiculo(object sender, RoutedEventArgs e)
+        private void btnCancelaCadastroVeiculo(object sender, RoutedEventArgs e)
         {
             TxtMarca.Clear();
             txtModelo.Clear();
@@ -40,8 +40,91 @@ namespace Locadora.Views
             TxtCor.Clear();
             TxtStatus.Clear();
             TxtPlaca.Focus();
+        }
 
+        private void CadastrarVeiculo_Click(object sender, RoutedEventArgs e)
+        {
+            Veiculo v = new Veiculo
+            {
+                NomeMarca = TxtMarca.Text,
+                NomeModelo = txtModelo.Text,
+                //ValorDia = Convert.ToDouble(CmbTipoVeiculo.Text),
+                Placa = TxtPlaca.Text,
+                Renavan = Convert.ToString(TxtRenavan.Text),
+                TipoVeiculo = CmbTipoVeiculo.Text,
+                TipoCombustivel = CmbTipoCombustivel.Text,
+                Cor = TxtCor.Text,
+                QntdPortas = Convert.ToInt32(TxtPortas.Text),
+                Ano = Convert.ToInt32(TxtAno.Text),
+                StatusVeiculo = TxtStatus.Text
+            };
 
+            if (VeiculoDAO.Cadastrar(v))
+            {
+                MessageBox.Show("Veículo cadastrado com sucesso!");
+                LimparFormulario();
+            }
+            else
+            {
+                MessageBox.Show("Veículo já existe!");
+            }
+        }
+
+        private void LimparFormulario()
+        {
+            TxtMarca.Clear();
+            txtModelo.Clear();
+            CmbTipoVeiculo.SelectedIndex = -1;
+            TxtPlaca.Clear();
+            TxtAno.Clear();
+            TxtRenavan.Clear();
+            TxtPortas.Clear();
+            CmbTipoCombustivel.SelectedIndex = -1;
+            TxtPotencia.Clear();
+            TxtCor.Clear();
+            TxtStatus.Clear();
+            TxtPlaca.Focus();
+        }
+
+        private void BuscarVeiculo_Click(object sender, RoutedEventArgs e)
+        {
+            if (!TxtRenavan.Text.Equals(""))
+            {
+                Veiculo v = new Veiculo
+                {
+                    Renavan = Convert.ToString(TxtRenavan.Text)
+                };
+                v = VeiculoDAO.BuscarVeiculoPorNumeroRenavam(v);
+                if (v != null)
+                {
+                    TxtMarca.Text = v.NomeMarca;
+                    txtModelo.Text = v.NomeModelo;
+                    CmbTipoVeiculo.Text = v.TipoVeiculo;
+                    TxtPlaca.Text = v.Placa;
+                    TxtAno.Text = Convert.ToString(v.Ano);
+                    TxtRenavan.Text = v.Renavan;
+                    TxtPortas.Text = Convert.ToString(v.QntdPortas);
+                    CmbTipoCombustivel.Text = v.TipoCombustivel;
+                    TxtPotencia.Text = Convert.ToString(v.Potencia);
+                    TxtCor.Text = v.Cor;
+                    TxtStatus.Text = v.StatusVeiculo;
+                    TxtPlaca.Text = v.Placa;
+                    //btnEditar.IsEnabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Veículo inexistente!", "Locador", MessageBoxButton.OK, MessageBoxImage.Information);
+                    LimparFormulario();
+                }
+            }
+        }
+
+        private void ExcluirVeiculo_Click(object sender, RoutedEventArgs e)
+        {
+            Veiculo v = new Veiculo();
+            VeiculoDAO.ExcluirVeiculo(v);
+            MessageBox.Show("Veiculo removido!", "Locador", MessageBoxButton.OK, MessageBoxImage.Information);
+            LimparFormulario();
         }
     }
 }
