84922ed223abdf1d24896639c93b9457b7362acd
WeslleyWar <55511759+WeslleyWar@users.noreply.github.com>
2020-10-05 22:33:36 -0300

    Versão correta do model de Funcionario
    O antigo Model relacionado a funcionario que foi upado é uma versão incorreta

--- Locadora/Locadora/Model/Funcionario.cs
+++ Locadora/Locadora/Model/Funcionario.cs
@@ -1,29 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Locadora.Models
+namespace Locadora.Model
 {
     [Table("Funcionarios")]
     class Funcionario : Pessoa
-    {   
-
+    {
         public Funcionario()
         {
             CriadoEm = DateTime.Now;
         }
+        public sealed class CmbGenero
+        {
+            public string Texto { get; set; }
+            public string Valor { get; set; }
 
-        [Key]
-        public int IdFuncionario { get; set; }
-        public DateTime DataContratacao { get; set; }
-        public string Cargo { get; set; }
-        public string Login { get; set; }
-        public string Senha { get; set; }
-        public float Salario { get; set; }
-        public DateTime CriadoEm { get; set; }
+            public override string ToString()
+            {
+                return this.Texto;
+            }
+        }
+        public String ContatoPessoal { get; set; }
+        public String CatHabilitacao { get; set; }
+        //public Boolean PermissãoDirigir { get; set; }
+        //public override string ToString()
+        //{
+        //    return $"Nome: {Nome} | CPF: {Cpf} | Telefone: {Telefone} |" +
+        //        $"Idade: {Idade} | Criado em: {CriadoEm}";
+        //}
     }
 }
