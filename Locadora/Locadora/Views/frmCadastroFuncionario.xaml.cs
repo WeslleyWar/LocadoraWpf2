a792bf6ee9cd7f7b2bbc6ebf6e50a67aa6783020
WeslleyWar <55511759+WeslleyWar@users.noreply.github.com>
2020-10-05 22:32:48 -0300

    Formulario certo de Funcionario
    O antigo formulado upado que era relacionado ao Funcionário é uma versão antiga upada acidentalmente

--- Locadora/Locadora/Views/frmCadastroFuncionario.xaml.cs
+++ Locadora/Locadora/Views/frmCadastroFuncionario.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Locadora.DAL;
+using Locadora.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,14 +30,88 @@ namespace Locadora.Views
         {
             TxtNome.Clear();
             TxtIdade.Clear();
-            CmbPermissao.SelectedIndex = -1;
+            CmbGenero.SelectedIndex = -1;
             TxtCpf.Clear();
             TxtDtNasc.Clear();
             TxtCnh.Clear();
             TxtEmail.Clear();
-            CmbPermissao.SelectedIndex = -1;
+            CmbHabilitacao.SelectedIndex = -1;
             TxtContatoPessoal.Clear();
             TxtCpf.Focus();
         }
+
+        private void LimparFormulario()
+        {
+            TxtNome.Clear();
+            TxtIdade.Clear();
+            TxtCpf.Clear();
+            TxtEmail.Clear();
+            TxtCnh.Clear();
+            TxtTelefone.Clear();
+            CmbHabilitacao.SelectedIndex = -1;
+            TxtContatoPessoal.Clear();
+            CmbGenero.SelectedIndex = -1;
+            TxtDtNasc.Clear();
+            TxtCpf.Focus();
+        }
+
+        private void btnSalvarFuncionario(object sender, RoutedEventArgs e)
+        {
+            Funcionario f = new Funcionario
+            {
+                Nome = TxtNome.Text,
+                Idade = Convert.ToInt32(TxtIdade.Text),
+                Cpf = TxtCpf.Text,
+                Email = TxtEmail.Text,
+                Cnh = TxtCnh.Text,
+                Telefone = TxtTelefone.Text,
+                Genero = CmbGenero.Text,
+                CatHabilitacao = CmbHabilitacao.Text,
+                DataNascimento = Convert.ToDateTime(TxtDtNasc.Text)
+            };
+
+            if (FuncionarioDAO.CadastrarFuncionario(f))
+            {
+                MessageBox.Show("Funcionario cadastrado com sucesso!");
+                LimparFormulario();
+            }
+            else
+            {
+                MessageBox.Show("Funcionario já existe!");
+            }
+        }
+
+        private void btnBuscarFuncionario(object sender, RoutedEventArgs e)
+        {
+            if (!TxtCpf.Text.Equals(""))
+            {
+                Funcionario f = new Funcionario
+                {
+                    Cpf = TxtCpf.Text
+                };
+                f = FuncionarioDAO.BuscarFuncionarioPorCpf(f);
+                if (f != null)
+                {
+                    TxtNome.Text = f.Nome;
+                    CmbGenero.Text = f.Genero;
+                    TxtCpf.Text = f.Cpf;
+                    CmbHabilitacao.Text = f.CatHabilitacao;
+                    TxtIdade.Text = Convert.ToString(Convert.ToInt32(f.Idade));
+                    TxtEmail.Text = f.Email;
+                    TxtDtNasc.Text = Convert.ToString(Convert.ToDateTime(f.DataNascimento));
+                    TxtCnh.Text = f.Cnh;
+                    TxtTelefone.Text = f.Telefone; 
+                    //TxtCriadoEm.Text = Convert.ToString(Convert.ToDateTime(f.CriadoEm));
+                    //btnEditar.IsEnabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("Funcionario inexistente!", "Locador", MessageBoxButton.OK, MessageBoxImage.Information);
+                    LimparFormulario();
+                }
+            }
+        }
+
+
     }
 }
